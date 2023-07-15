0c592f8ed3df8eb91a8c4138f2ef3afa7bf4830e
madflow <madflow@web.de>
2023-07-15 16:20:07 +0200

    make powered by logo configurable

--- components/Navigator.tsx
+++ components/Navigator.tsx
@@ -8,6 +8,9 @@ import SearchBox from "@components/Searchbox";
 export default function Navigator() {
   const router = useRouter();
 
+  const showPoweredBy =
+    process.env.NEXT_PUBLIC_SHOW_POWERED_BY === "1" ? true : false;
+
   return (
     <Pane
       width={240}
@@ -76,20 +79,22 @@ export default function Navigator() {
       </Pane>
 
       <Pane borderTop>
-        <a
-          href="https://vercel.com?utm_source=ritz078&utm_campaign=oss"
-          target="_blank"
-        >
-          <img
-            src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
-            alt="Vercel"
-            style={{
-              height: 40,
-              display: "block",
-              margin: "10px auto 0"
-            }}
-          />
-        </a>
+        {showPoweredBy && (
+          <a
+            href="https://vercel.com?utm_source=ritz078&utm_campaign=oss"
+            target="_blank"
+          >
+            <img
+              src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
+              alt="Vercel"
+              style={{
+                height: 40,
+                display: "block",
+                margin: "10px auto 0"
+              }}
+            />
+          </a>
+        )}
         <Heading size={400} paddingY={15} textAlign="center">
           Created by{" "}
           <a href="https://twitter.com/ritz078" target="_blank">
