8c8fe958c11008b33ca6ea0c51a7b1b5695b58c9
madflow <madflow@web.de>
2023-07-16 15:09:30 +0200

    remove gofmt.js

--- pages/json-to-go.tsx
+++ pages/json-to-go.tsx
@@ -2,11 +2,10 @@ import ConversionPanel from "@components/ConversionPanel";
 import { useCallback } from "react";
 import * as React from "react";
 import jsonToGo from "json-to-go";
-import gofmt from "gofmt.js";
 
 export default function JsonToGo() {
   const transformer = useCallback(({ value }) => {
-    return gofmt(jsonToGo(value).go);
+    return jsonToGo(value).go;
   }, []);
 
   return (
